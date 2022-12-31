65f18991c05357db0876386b07b486bb2c5d6e76
Savio <dsaviosnogueira@gmail.com>
2022-12-31 01:16:19 -0300

    :sparkles: feat: function finished task conclued

--- toDoList/src/components/Task/index.tsx
+++ toDoList/src/components/Task/index.tsx
@@ -2,11 +2,26 @@ import styles from "./style.module.css";
 
 import { BsTrash } from "react-icons/bs";
 
+import { useState } from "react";
+
 export function Task({ task, handleDelete }) {
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
+
+  function handleTask() {
+    console.log(task.isCompleted);
+    if (isCompleted) {
+      return setIsCompleted(false);
+    } else {
+      return setIsCompleted(true);
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <input type="checkbox" readOnly={task.isCompleted} />
-      <p> {task.task} </p>
+      <input type="checkbox" onChange={handleTask} />
+      <p className={isCompleted ? styles.checked : styles.noChecked}>
+        {task.task}
+      </p>
 
       <button onClick={() => handleDelete(task)}>
         <BsTrash size={16} />
--- toDoList/src/page/Home/index.tsx
+++ toDoList/src/page/Home/index.tsx
@@ -28,7 +28,6 @@ export function Home() {
     const newListTask = tasks.filter((item) => {
       return item.task !== task.task;
     });
-
     setTasks(newListTask);
   }
 
